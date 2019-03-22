b7cbe4f350c8b2fa0ad298a01039fb8b4d3b2f81
alumno <alumno@utnfra.com.ar>
2019-03-21 22:04:29 -0300

    Binding completo

--- CicloDeVida/src/app/componentes/login/login.component.ts
+++ CicloDeVida/src/app/componentes/login/login.component.ts
@@ -9,12 +9,19 @@ import { Usuario } from 'src/app/clases/usuario';
 export class LoginComponent implements OnInit {
 
   public usuario: Usuario;
+  public guardado: boolean;
 
   constructor() {
     this.usuario = new Usuario();
+    this.guardado = false;
   }
 
   ngOnInit() {
+  }
+
+  guardar() {
+    this.guardado = true;
+    console.info("el objeto",this.usuario)
   }
 
 }
