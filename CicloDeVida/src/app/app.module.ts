ebdd7d644cdc5708f101c13be781745058f905cf
alumno <alumno@utnfra.com.ar>
2019-03-21 21:31:53 -0300

    Cargo formulario login con binding

--- CicloDeVida/src/app/app.module.ts
+++ CicloDeVida/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
@@ -24,7 +25,8 @@ const miRuteo = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(miRuteo)
+    RouterModule.forRoot(miRuteo),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CicloDeVida/src/app/componentes/login/login.component.ts
+++ CicloDeVida/src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/clases/usuario';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public usuario: Usuario;
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit() {
   }
